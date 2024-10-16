dc2ae9793661e3859d52ea2b3ddacafce5781b95
vreztsov <31355847+vreztsov@users.noreply.github.com>
2024-10-17 01:58:25 +0500

    Paging RPA

--- app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
@@ -99,8 +99,6 @@ class FeedFragment : Fragment() {
             adapter.loadStateFlow.collectLatest {
                 binding.swiperefresh.isRefreshing =
                     it.refresh is LoadState.Loading
-                            || it.append is LoadState.Loading
-                            || it.prepend is LoadState.Loading
             }
         }
         viewModel.dataState.observe(viewLifecycleOwner) { state ->
