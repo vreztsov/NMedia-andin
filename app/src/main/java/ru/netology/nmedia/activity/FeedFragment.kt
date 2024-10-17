421ab76bae44d57df1f4f81b90d11c0018c6d28e
vreztsov <31355847+vreztsov@users.noreply.github.com>
2024-10-17 21:58:08 +0300

    Fix FeedFragment.kt

--- app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
@@ -1,10 +1,12 @@
 package ru.netology.nmedia.activity
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -30,6 +32,7 @@ class FeedFragment : Fragment() {
 
     private val viewModel: PostViewModel by activityViewModels()
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
